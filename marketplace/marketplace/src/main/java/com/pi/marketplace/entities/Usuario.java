ea556f1b63620aa6ef7da9bc675da5df7f9bd91b
Karine Viana Caldas | KVIANA83 <129767662+KVIANA83@users.noreply.github.com>
2023-10-11 22:48:02 -0300

    Atualizar o Usuario.java

--- marketplace/marketplace/src/main/java/com/pi/marketplace/entities/Usuario.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/entities/Usuario.java
@@ -12,13 +12,29 @@ public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = 'id_usuario')
     private int idUsuario;
+
+    @Column(name = 'data_nascimento')
     private int dataNascimento;
+
+    @Column
     private String telefone;
+
+    @Column(name = 'nome_completo')
     private String nomeCompleto;
+
+    @Column(name = 'nome_social')
     private String nomeSocial;
+
+    @Column
     private String endereco;
+
+    @Column
     private String email;
+
+    @Column
     private String senha;
 
     // ID Usuário
