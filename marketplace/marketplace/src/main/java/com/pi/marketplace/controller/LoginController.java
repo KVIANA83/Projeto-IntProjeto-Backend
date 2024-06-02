369a719694c56723c13a951d5ee6ae25b181b777
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-02 13:54:56 -0300

    Update LoginController.java

--- marketplace/marketplace/src/main/java/com/pi/marketplace/controller/LoginController.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/controller/LoginController.java
@@ -4,11 +4,21 @@ import com.pi.marketplace.dto.LoginDTO;
 import com.pi.marketplace.service.EmpreendedorService;
 import com.pi.marketplace.service.UsuarioService;
 import jakarta.validation.Valid;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@Tag(name = "Tem no Bairro Login Controller API")
+@Slf4j
 @RestController
 @AllArgsConstructor
 @RequestMapping("/login")
