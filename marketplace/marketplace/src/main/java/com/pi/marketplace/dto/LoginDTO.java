fba071fc95fc4e6b626dac6f6fdd3ee7ca845824
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-02 14:34:09 -0300

    Atualizar o LoginDTO.java
    adicionando documentação

--- marketplace/marketplace/src/main/java/com/pi/marketplace/dto/LoginDTO.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/dto/LoginDTO.java
@@ -1,6 +1,7 @@
 package com.pi.marketplace.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +13,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class LoginDTO {
 
+    @Schema(name = "email", 
+            description = "Login do usuário cadastrado", 
+            example = "usuarioteste@email.com", 
+            required = true)
     @NotBlank(message = "email é um campo obrigatório!")
     private String email;
 
+    @Schema(name = "senha", 
+            description = "Senha cadastrada pelo usuário", 
+            example = "SeNHa.123", 
+            required = true)
     @NotBlank(message = "senha é um campo obrigatório!")
     private String senha;
 }
