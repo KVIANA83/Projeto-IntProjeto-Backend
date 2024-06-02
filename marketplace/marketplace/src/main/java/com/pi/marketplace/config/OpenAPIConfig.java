9aed849090227fa16d65d435b2b0e64817e03b93
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-02 13:47:47 -0300

    Update OpenAPIConfig.java

--- marketplace/marketplace/src/main/java/com/pi/marketplace/config/OpenAPIConfig.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/config/OpenAPIConfig.java
@@ -1,1 +1,22 @@
+package com.pi.marketplace.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@OpenAPIDefinition
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI stringOpenAI() {
+        return new OpenAPI().info(new Info()
+                                    .title("TEM no Bairro API")
+                                    .description(" ")
+                                    .version(""));
+    }
+
+}
