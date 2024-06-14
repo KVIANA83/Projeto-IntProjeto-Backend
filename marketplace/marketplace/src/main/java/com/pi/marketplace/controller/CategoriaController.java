9e59e7b920537df20c2f83f97bcd46cab7a65636
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-14 19:00:46 -0300

    Atualizar o CategoriaController.java
    adicionando documentacao swagger

--- marketplace/marketplace/src/main/java/com/pi/marketplace/controller/CategoriaController.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/controller/CategoriaController.java
@@ -8,8 +8,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
+@Tag(name = "Categorias Controller API")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/categorias")
@@ -18,12 +26,31 @@ public class CategoriaController {
     private CategoriaService categoriaService;
 
 
+    @Operation(
+        summary = "Listar todas as categorias",
+        description = "Endpoint para listar todas as categorias disponíveis na plataforma",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = CategoriaDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @GetMapping("/listar")
     public ResponseEntity<List<CategoriaDTO>> listarCategorias() {
 
         return ResponseEntity.ok(categoriaService.listarTodasCategorias());
     }
 
+    @Operation(
+        summary = "Salvar uma nova categoria",
+        description = "Endpoint para criar uma nova categoria",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Categoria criada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @PostMapping("/salvar")
     public ResponseEntity<Void> criarCategoria(@Valid @RequestBody CreateCategoriaDTO categoriaDTO) {
 
@@ -32,6 +59,18 @@ public class CategoriaController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Deletar uma categoria",
+        description = "Endpoint para deletar uma categoria",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Categoria deletada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID da categoria", example = "1")
+        }
+    )
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> criarCategoria(@PathVariable("id") Integer idCategoria) {
 
