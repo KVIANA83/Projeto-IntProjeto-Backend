c001736e76f5d1fab0bf495fcba7bc1803af4590
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-14 19:10:37 -0300

    Atualizar o EmpreendedorController.java
    adicionando documentacao swagger

--- marketplace/marketplace/src/main/java/com/pi/marketplace/controller/EmpreendedorController.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/controller/EmpreendedorController.java
@@ -9,8 +9,16 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
+@Tag(name = "Empreendedores Controller API")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/empreendedores")
@@ -19,12 +27,31 @@ public class EmpreendedorController {
     private EmpreendedorService empreendedorService;
 
 
+    @Operation(
+        summary = "Listar todos os empreendedores",
+        description = "Endpoint para listar todos os empreendedores na plataforma",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = EmpreendedorDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @GetMapping("/listar")
     public ResponseEntity<List<EmpreendedorDTO>> listarEmpreendedores() {
 
         return ResponseEntity.ok(empreendedorService.listarTodos());
     }
 
+    @Operation(
+        summary = "Salvar um novo empreendedor",
+        description = "Endpoint para criar um novo empreendedor",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Empreendedor criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @PostMapping("/salvar")
     public ResponseEntity<Void> salvarEmpreendedor(@Valid @RequestBody CreateEmpreendedorDTO createEmpreendedorDTO) {
 
@@ -33,12 +60,37 @@ public class EmpreendedorController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Buscar um empreendedor pelo ID",
+        description = "Endpoint para buscar um empreendedor pelo seu ID",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = Empreendedor.class))),
+            @ApiResponse(responseCode = "404", description = "Empreendedor não encontrado", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do empreendedor", example = "1")
+        }
+    )
     @GetMapping("/buscar/{id}")
     public ResponseEntity<Empreendedor> buscarEmpreendedorPeloId(@PathVariable("id") Integer id) {
 
         return ResponseEntity.ok(empreendedorService.pegarEmpreendedorPeloId(id));
     }
 
+    @Operation(
+        summary = "Atualizar um empreendedor",
+        description = "Endpoint para atualizar um empreendedor existente",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Empreendedor atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do empreendedor", example = "1")
+        }
+    )
     @PostMapping("/atualizar/{id}")
     public ResponseEntity<Void> atualizarEmpreendedor(@PathVariable("id") Integer id,
                                                       @Valid @RequestBody CreateEmpreendedorDTO createEmpreendedorDTO) {
@@ -48,6 +100,18 @@ public class EmpreendedorController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Deletar um empreendedor",
+        description = "Endpoint para deletar um empreendedor existente",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Empreendedor deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do empreendedor", example = "1")
+        }
+    )
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<Void> deletarEmpreendedor(@PathVariable("id") Integer id) {
 
