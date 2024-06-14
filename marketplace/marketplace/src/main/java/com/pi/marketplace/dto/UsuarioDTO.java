add659969c5bb09c6447f7e165692d70182aea8d
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-14 19:25:38 -0300

    Atualizar o UsuarioDTO.java
    adicionando documentacao swagger

--- marketplace/marketplace/src/main/java/com/pi/marketplace/dto/UsuarioDTO.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/dto/UsuarioDTO.java
@@ -1,5 +1,6 @@
 package com.pi.marketplace.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -13,11 +14,26 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO para retornar informações do usuário")
 public class UsuarioDTO {
 
+    @Schema(description = "Telefone do usuário", 
+			example = "+5511999999999")
     private String telefone;
+
+    @Schema(description = "Nome completo do usuário", 
+			example = "João Silva")
     private String nomeCompleto;
+
+    @Schema(description = "Nome social do usuário, se aplicável", 
+			example = "Joãozinho Silva")
     private String nomeSocial;
+
+    @Schema(description = "Endereço do usuário", 
+			example = "Rua das Flores, 123, São Paulo, SP")
     private String endereco;
+
+    @Schema(description = "Email do usuário", 
+			example = "joao.silva@example.com")
     private String email;
 }
