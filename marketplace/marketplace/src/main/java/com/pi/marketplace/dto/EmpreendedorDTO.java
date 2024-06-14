e92002f5eb6e5741b878246ba6742a2a981d2e17
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-14 19:40:44 -0300

    Atualizar o EmpreendedorDTO.java
    adicionando documentacao swagger

--- marketplace/marketplace/src/main/java/com/pi/marketplace/dto/EmpreendedorDTO.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/dto/EmpreendedorDTO.java
@@ -3,6 +3,7 @@ package com.pi.marketplace.dto;
 import com.pi.marketplace.model.Empreendedor;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,12 +15,27 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "DTO para retornar informações do empreendedor")
 public class EmpreendedorDTO {
 
+    @Schema(description = "Telefone do empreendedor", 
+			example = "+5511999999999")
     private String telefone;
+
+    @Schema(description = "Nome completo do empreendedor", 
+			example = "João Silva")
     private String nomeCompleto;
+
+    @Schema(description = "Razão social do empreendedor", 
+			example = "Empresa XYZ LTDA")
     private String razaoSocial;
+
+    @Schema(description = "Endereço do empreendedor", 
+			example = "Rua das Flores, 123, São Paulo, SP")
     private String endereco;
+
+    @Schema(description = "Email do empreendedor", 
+			example = "joao.silva@empresa.com")
     private String email;
 
 }
