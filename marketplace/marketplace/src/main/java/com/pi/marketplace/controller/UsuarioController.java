dc26ced03ee353f8e40da747b3d0fa73a6c810f7
Karine Viana Caldas <129767662+KVIANA83@users.noreply.github.com>
2024-06-14 19:17:33 -0300

    Atualizar o UsuarioController.java
    adicionando documentacao swagger

--- marketplace/marketplace/src/main/java/com/pi/marketplace/controller/UsuarioController.java
+++ marketplace/marketplace/src/main/java/com/pi/marketplace/controller/UsuarioController.java
@@ -14,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
 import java.util.List;
 
+@Tag(name = "Usuarios Controller API")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/usuarios")
@@ -24,18 +32,51 @@ public class UsuarioController {
 
     private UsuarioService usuarioService;
 
+
+    @Operation(
+        summary = "Listar todos os usuários",
+        description = "Endpoint para listar todos os usuários na plataforma",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = UsuarioDTO.class))),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @GetMapping("/listar")
     public ResponseEntity<List<UsuarioDTO>> listarUsuarios() {
 
         return ResponseEntity.ok(usuarioService.listarTodos());
     }
 
+    @Operation(
+        summary = "Buscar um usuário pelo ID",
+        description = "Endpoint para buscar um usuário pelo seu ID",
+        responses = {
+            @ApiResponse(responseCode = "200", description = "Requisição feita com sucesso", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = Usuario.class))),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do usuário", example = "1")
+        }
+    )
     @GetMapping("/buscar/{id}")
     public ResponseEntity<Usuario> getUsuario(@PathVariable Integer id) {
 
         return ResponseEntity.ok(usuarioService.pegarUsuarioPeloId(id));
     }
 
+    @Operation(
+        summary = "Criar um novo usuário",
+        description = "Endpoint para criar um novo usuário",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Usuário criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        }
+    )
     @PostMapping("/criar")
     public ResponseEntity<Void> criarUsuario(@Valid @RequestBody CreateUsuarioDTO usuarioDTO) {
 
@@ -44,6 +85,18 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Atualizar um usuário",
+        description = "Endpoint para atualizar um usuário existente",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Usuário atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do usuário", example = "1")
+        }
+    )
     @PostMapping("/atualizar/{id}")
     public ResponseEntity<Void> atualizarUsuario(@PathVariable Integer id,
             @Valid @RequestBody CreateUsuarioDTO usuarioAtualizar) {
@@ -53,6 +106,18 @@ public class UsuarioController {
         return ResponseEntity.noContent().build();
     }
 
+    @Operation(
+        summary = "Deletar um usuário",
+        description = "Endpoint para deletar um usuário existente",
+        responses = {
+            @ApiResponse(responseCode = "204", description = "Usuário deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na requisição", 
+                content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class)))
+        },
+        parameters = {
+            @Parameter(name = "id", description = "ID do usuário", example = "1")
+        }
+    )
     @DeleteMapping("/deletar/{id}")
     public ResponseEntity<?> deletarUsuario(@PathVariable Integer id) {
 
